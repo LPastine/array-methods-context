9992782ce1371b79095ab23fa582e29431492e16
LPastine <lucianopastine.tech@gmail.com>
2022-09-23 15:17:49 -0300

    feat(actions): added state actions

---
+++ src/state/actions.ts
@@ -0,0 +1,41 @@
+import { User } from "./state";
+
+export enum ActionType {
+  AddUser,
+  DoubleMoney,
+  ShowOnlyMillionaires,
+  SortByRichest,
+  CalculateWealth,
+}
+
+export interface AddUser {
+  type: ActionType.AddUser,
+  payload: User,
+}
+
+export interface DoubleMoney {
+  type: ActionType.DoubleMoney,
+  payload: User[],
+}
+
+export interface ShowOnlyMillionaires {
+  type: ActionType.ShowOnlyMillionaires,
+  payload: User[],
+}
+
+export interface SortByRichest {
+  type: ActionType.SortByRichest,
+  payload: User[],
+}
+
+export interface CalculateWealth {
+  type: ActionType.CalculateWealth,
+  payload: User[],
+}
+
+export type UserActions = 
+  | AddUser 
+  | DoubleMoney 
+  | ShowOnlyMillionaires 
+  | SortByRichest 
+  | CalculateWealth
